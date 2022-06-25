3e26067ab61ea0d90d0365cb0af4a0f25370fa8f
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2022-06-26 02:22:48 +0300

    Changes: - Class Radio added - 100% tests added

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,52 @@
 package ru.netology.domain;
 
 public class Radio {
+    private int currentStationNumber;
+    private int currentVolume;
 
+    public void nextStation() {
+        if (currentStationNumber == 9) {
+            currentStationNumber = 0;
+        } else {
+            currentStationNumber++;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStationNumber == 0) {
+            currentStationNumber = 9;
+        } else {
+            currentStationNumber--;
+        }
+    }
+
+    public void setCurrentStationNumber(int currentStationNumber) {
+        if (currentStationNumber < 0) {
+            return;
+        }
+        if (currentStationNumber > 9) {
+            return;
+        }
+        this.currentStationNumber = currentStationNumber;
+    }
+
+    public int getCurrentStationNumber() {
+        return currentStationNumber;
+    }
+
+    public void plusVolume() {
+        if (currentVolume != 10) {
+            currentVolume++;
+        }
+    }
+
+    public void minusVolume() {
+        if (currentVolume != 0) {
+            currentVolume--;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,8 +1,66 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataCurrentStation.csv")
+    void setCurrentStationNumberTest(int input, int expected) {
+        Radio radio = new Radio();
 
+        radio.setCurrentStationNumber(input);
+        int actual = radio.getCurrentStationNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataNextStation.csv")
+    void nextStationTest(int input, int expected) {
+        Radio radio = new Radio();
+
+        radio.setCurrentStationNumber(input);
+        radio.nextStation();
+        int actual = radio.getCurrentStationNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataPrevStation.csv")
+    void prevStationTest(int input, int expected) {
+        Radio radio = new Radio();
+
+        radio.setCurrentStationNumber(input);
+        radio.prevStation();
+        int actual = radio.getCurrentStationNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void plusMinusVolumeTest() {
+        Radio radio = new Radio();
+
+        assertEquals(0, radio.getCurrentVolume());
+        for (int newVolume = 1; newVolume <= 10; newVolume++) {
+            radio.plusVolume();
+            int actual = radio.getCurrentVolume();
+            assertEquals(newVolume, actual);
+        }
+        radio.plusVolume();
+        assertEquals(10, radio.getCurrentVolume());
+        for (int newVolume = 9; newVolume >= 0; newVolume--) {
+            radio.minusVolume();
+            int actual = radio.getCurrentVolume();
+            assertEquals(newVolume, actual);
+        }
+        radio.minusVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
 }
